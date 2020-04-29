871a743a81e1041f6f1272ceb346125edcc5a5f3
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-04-29 14:35:56 +0100

    Run terraform format tool on codebase

--- irsa.tf
+++ irsa.tf
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "external_dns_irsa" {
   }
 
   statement {
-    actions   = [
+    actions = [
       "route53:ListHostedZones",
       "route53:ListResourceRecordSets",
     ]
