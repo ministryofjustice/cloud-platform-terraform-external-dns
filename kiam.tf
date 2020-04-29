0d4af95cbc4cb676d7d420de0662e064dbff979b
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2020-04-29 14:31:53 +0100

    Amend kiam roles so they fit with module
    As the iam policies were using direct data sources this module couldn't work. Now they are passed in when the module is called and stored in variables

--- kiam.tf
+++ kiam.tf
@@ -1,19 +1,20 @@
-
 # IAM Role for ServiceAccounts: Kops clusters 
 
 data "aws_iam_policy_document" "external_dns_assume" {
+  count = var.eks ? 0 : 1
   statement {
     actions = ["sts:AssumeRole"]
 
     principals {
       type        = "AWS"
-      identifiers = [data.aws_iam_role.nodes.arn]
+      identifiers = [var.iam_role_nodes]
     }
   }
 }
 
 resource "aws_iam_role" "external_dns" {
-  name               = "external-dns.${data.terraform_remote_state.cluster.outputs.cluster_domain_name}"
+  count              = var.eks ? 0 : 1
+  name               = "external-dns.${var.cluster_domain_name}"
   assume_role_policy = data.aws_iam_policy_document.external_dns_assume.json
 }
 
@@ -21,17 +22,9 @@ data "aws_iam_policy_document" "external_dns" {
   statement {
     actions = ["route53:ChangeResourceRecordSets"]
 
-    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-    # force an interpolation expression to be interpreted as a list by wrapping it
-    # in an extra set of list brackets. That form was supported for compatibility in
-    # v0.11, but is no longer supported in Terraform v0.12.
-    #
-    # If the expression in the following list itself returns a list, remove the
-    # brackets to avoid interpretation as a list of lists. If the expression
-    # returns a single list item then leave it as-is and remove this TODO comment.
     resources = [format(
       "arn:aws:route53:::hostedzone/%s",
-      terraform.workspace == local.live_workspace ? "*" : data.terraform_remote_state.cluster.outputs.hosted_zone_id,
+      terraform.workspace == local.live_workspace ? "*" : var.hostedzone,
     )]
   }
 
@@ -46,6 +39,7 @@ data "aws_iam_policy_document" "external_dns" {
 }
 
 resource "aws_iam_role_policy" "external_dns" {
+  count  = var.eks ? 0 : 1
   name   = "route53"
   role   = aws_iam_role.external_dns.id
   policy = data.aws_iam_policy_document.external_dns.json
