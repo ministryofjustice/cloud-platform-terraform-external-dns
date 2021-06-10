251adf0f0cb621fa0bad90df5f69cccfe4fe2ea3
Alejandro Garrido Mota <alejandro.garrido@digital.justice.gov.uk>
2021-06-10 14:14:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "helm_release" "external_dns" {
   chart      = "external-dns"
   repository = "https://charts.bitnami.com/bitnami"
   namespace  = "kube-system"
-  version    = "5.0.4"
+  version    = "5.0.3"
 
   values = [templatefile("${path.module}/templates/values.yaml.tpl", {
     domainFilters = lookup(local.domainfilters, terraform.workspace, local.domainfilters["default"])
