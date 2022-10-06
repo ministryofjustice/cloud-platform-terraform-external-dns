7dc82c740ae65edc0fdfd090bc724a33232074a6
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-10-06 09:48:46 +0100

    no pinning in modules

--- versions.tf
+++ versions.tf
@@ -1,16 +1,14 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.14"
   required_providers {
     aws = {
       source = "hashicorp/aws"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.6.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = "1.11.2"
     }
   }
 }
