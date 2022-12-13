d2df530064879454b52e581bc6d5c65294fa24b0
jakemulley <jakemulley@users.noreply.github.com>
2022-12-13 17:29:35 +0000

    Refactor to meet standardised Terraform structure

--- versions.tf
+++ versions.tf
@@ -2,13 +2,16 @@ terraform {
   required_version = ">= 0.14"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = ">=4.24.0"
     }
     helm = {
       source  = "hashicorp/helm"
+      version = ">=2.6.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
+      version = ">=1.13.2"
     }
   }
 }
