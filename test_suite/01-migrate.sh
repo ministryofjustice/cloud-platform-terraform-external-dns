e2f066ee03358452e11892924b5c9a89d7ed33fd
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2025-02-25 22:03:03 +0000

    feat: 🎸 wip

--- test_suite/01-migrate.sh
+++ test_suite/01-migrate.sh
@@ -13,12 +13,6 @@ upsert_record() {
     WEIGHT=$(echo "$TXT_OBJ" | jq '.Weight')
 
 
-    if [ -n "${WEIGHT}" ]; then
-        echo "inside weight"
-        WEIGHT="100"
-    fi
-
-    echo "WEIGHT $WEIGHT"
 
     SET_IDENTIFIER=$(echo "$TXT_OBJ" | jq '.SetIdentifier')
 
@@ -26,10 +20,12 @@ upsert_record() {
 
 
     if [ -n "${SET_IDENTIFIER}" ]; then
-        SET_IDENTIFIER=$(echo '"$(date +%s)"')
+        # An error occurred (InvalidInput) when calling the ChangeResourceRecordSets operation: Invalid request: Expected exactly one of [Weight, Region, Failover, GeoLocation, MultiValueAnswer, GeoProximityLocation, or CidrRoutingConfig], but found none in Change with [Action=UPSERT, Name=_external_dns.cname.alertmanager.cp-2502-1521.cloud-platform.service.justice.gov.uk., Type=TXT, SetIdentifier=monitoring-alertmanager-proxy-oauth2-proxy-green]
 
-        # ID=$(echo $RESOURCE_RECORD_VALUE | sed 's/.*resource=ingress\/\(.*\)/\1/' | sed 's/\//-/' | sed 's/\\""//')
-        # SET_IDENTIFIER="$ID-green"
+
+        # An error occurred (InvalidChangeBatch) when calling the ChangeResourceRecordSets operation: [RRSet with DNS name _external_dns.cname.\052.apps.cp-2502-1521.cloud-platform.service.justice.gov.uk., type TXT, SetIdentifier ingress-controllers-nginx-ingress-default-controller-green, and Region Name=eu-west-2 cannot be created because a non-latency RRSet with the same name and type already exists.]
+        ID=$(echo $RESOURCE_RECORD_VALUE | sed -r 's/.*resource=(ingress|service)\/(.*)\\\"\"/\2/' | sed 's/\//-/')
+        SET_IDENTIFIER='"'"$ID-green"'"'
     fi
 
     echo "SET ID $SET_IDENTIFIER"
@@ -39,9 +35,31 @@ upsert_record() {
     if echo $NAME | grep -vq "_external_dns.cname"; then
         UPDATED_NAME=$(echo "$NAME" | sed 's/_external_dns\./_external_dns.cname./g')
 
-        aws route53 change-resource-record-sets \
-            --hosted-zone-id $HOSTED_ZONE_ID \
-            --change-batch '
+
+        if [ -n "${WEIGHT}" ]; then
+            aws route53 change-resource-record-sets \
+                --hosted-zone-id $HOSTED_ZONE_ID \
+                --change-batch '
+          {
+            "Comment": "Testing upserting a record set"
+            ,"Changes": [{
+              "Action"              : "UPSERT"
+              ,"ResourceRecordSet"  : {
+                "Name"              : '"$UPDATED_NAME"'
+                ,"Type"             : "TXT"
+                ,"TTL"              : 300
+                ,"SetIdentifier"    : '"$SET_IDENTIFIER"'
+                ,"ResourceRecords"  : [{
+                            "Value"         : '$RESOURCE_RECORD_VALUE'
+                        }]
+                    }
+            }]
+          }
+            '
+        else
+            aws route53 change-resource-record-sets \
+                --hosted-zone-id $HOSTED_ZONE_ID \
+                --change-batch '
           {
             "Comment": "Testing upserting a record set"
             ,"Changes": [{
@@ -58,7 +76,9 @@ upsert_record() {
                     }
             }]
           }
-        '
+            '
+
+        fi
     fi
 
 }
